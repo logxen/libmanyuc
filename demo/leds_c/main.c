251d82e6d9d6a709eaf1c6110a1c267dbe01d4b8
Margarita Manterola <margamanterola@gmail.com>
2012-05-04 21:11:25 -0300

    Modifico el ejemplo leds_c para que tome las nuevas ubicaciones

--- demo/leds_c/main.c
+++ demo/leds_c/main.c
@@ -21,26 +21,33 @@
 #include "libmanyuc.h"
 
 /* This example turns all 4 leds on and then off */
-
 int main(void) {
 
-	Pin_t leds[] = { Pin_Get(LED4), Pin_Get(LED3), 
-		Pin_Get(LED2), Pin_Get(LED1) };
-	int nleds = 4;
+	// Access 4 leds predefined as LED1, LED2, LED3, LED4
+	Pin_t leds[] = { 
+		Pin_Get(LED1), 
+		Pin_Get(LED2), 
+		Pin_Get(LED3), 
+		Pin_Get(LED4) 
+	};
+	int nleds = 4, i = 0;
 
-	int i;
+	// Set all pins as output
 	for (i = 0; i < nleds; i++) {
 		Pin_Output(leds[i]);
 	}
 
+	// Turn all leds on and then off, 
+	// with a delay of 0.2s among operations.
+    // This goes on indefinitely
     while(1) {
 		for (i = 0; i < nleds; i++) {
 			Pin_On(leds[i]);
-        	wait(0.2);         // Delay 100 Msec
+        	wait(0.2);
 		}
 		for (i = 0; i < nleds; i++) {
 			Pin_Off(leds[i]);
-        	wait(0.2);         // Delay 100 Msec
+        	wait(0.2);
 		}
     }
 }
