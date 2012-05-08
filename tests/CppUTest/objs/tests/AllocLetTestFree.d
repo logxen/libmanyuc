0fe6cf562c2aefc43ab6454938541d4aa0c068bf
Margarita Manterola <margamanterola@gmail.com>
2012-05-08 20:15:00 -0300

    Fix the Makefile in the test directory so that irrelevant code can be cleaned and not commited by using the dist-clean target. Fixes issue 1.

--- tests/CppUTest/objs/tests/AllocLetTestFree.d
+++
@@ -1,9 +0,0 @@
-objs/tests/AllocLetTestFree.o: tests/AllocLetTestFree.c \
- include/CppUTest/MemoryLeakDetectorMallocMacros.h \
- include/CppUTest/StandardCLibrary.h tests/AllocLetTestFree.h
-
-include/CppUTest/MemoryLeakDetectorMallocMacros.h:
-
-include/CppUTest/StandardCLibrary.h:
-
-tests/AllocLetTestFree.h:
