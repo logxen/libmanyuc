549ca40eab16c84f62b73aaf450dd90ede321127
Margarita Manterola <margamanterola@gmail.com>
2012-05-12 13:47:21 -0300

    Add toggle_pin example to measure performance

---
+++ demo/toggle_pin_c/main.c
@@ -0,0 +1,34 @@
+/*
+ * libmanyuc - Turn leds on in C example
+ * Copyright (C) 2012 - Margarita Manterola Rivero
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, 
+ * MA 02110-1301 USA
+ */
+
+#include "libmanyuc.h"
+
+/* This examples toggles pin 11 on and off as fast as possible. */
+int main(void) {
+
+	Pin_t my_pin = Pin_Get(P11);
+	Pin_Output(my_pin);
+
+	// Turn a led on and off indefinitely.
+    while(1) {
+		Pin_On(my_pin);
+		Pin_Off(my_pin);
+    }
+}
