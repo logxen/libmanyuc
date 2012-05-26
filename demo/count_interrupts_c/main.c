f1a85bf998a16d3206a658aba903446432140c82
Margarita Manterola <margamanterola@gmail.com>
2012-05-26 16:52:05 -0300

    Tidy up count interrupts example

--- demo/count_interrupts_c/main.c
+++ demo/count_interrupts_c/main.c
@@ -1,5 +1,5 @@
 /*
- * libmanyuc - Turn leds on in C example
+ * libmanyuc - Measure speed of interrupt handling in C
  * Copyright (C) 2012 - Margarita Manterola Rivero
  *
  * This library is free software; you can redistribute it and/or
@@ -21,33 +21,62 @@
 #include "libmanyuc.h"
 
 uint32_t i;
-Bus_t leds;
+Pin_t debug;
 
-// Interrupt handler: increments the counter and shows the number
-// divided by (256*256).
+// Interrupt counter, toggles debug pin when 100k interrupts happen.
 void count(void) {
 	i++;
-	Bus_Write(leds, (i>>16));
+	if (i >= 100000) {
+		Pin_Toggle(debug);
+		i = 0;
+	}
 }
 
-/* This example counts the number of interrupts received on pin 10,
- * which is toggled by the program in an infinite loop, showing a
- * number in binary using the leds. */
+/* This example counts the number of interrupts received on several pins, which
+ * are toggled by the program in an infinite loop, toggling a certain pin after
+ * 100k interrupts. */
 int main(void) {
 
-	// Creates a bus with the 4 leds
-	leds = Bus_Get (4, LED4, LED3, LED2, LED1);
-	Pin_t in = Pin_Get(P10);
+	debug1 = Pin_Get (P5);
 
-	Bus_Output(leds);
-	Pin_Output(in);
+	// This indexes indicate how many interrupts are configured
+	// And how many are going to be activated in the program code.
+	int start_config = 0;
+	int config_ints = 16;
+	int start_active = 4;
+	int active_ints = 9;
 
-	Pin_Int_Attach(in, count, IOIntFall);
+	// Pins used for interrupts
+	Pin_t ins[16];
 
-	int i = 0;
-	// Show a binary combination of leds
+	ins[0] = Pin_Get(ARM_P0_0);
+	ins[1] = Pin_Get(ARM_P0_1);
+	ins[2] = Pin_Get(ARM_P0_4);
+	ins[3] = Pin_Get(ARM_P0_5);
+	ins[4] = Pin_Get(ARM_P0_23);
+	ins[5] = Pin_Get(ARM_P0_24);
+	ins[6] = Pin_Get(ARM_P0_25);
+	ins[7] = Pin_Get(ARM_P0_26);
+	ins[8] = Pin_Get(ARM_P2_0);
+	ins[9] = Pin_Get(ARM_P2_1);
+	ins[10] = Pin_Get(ARM_P2_2);
+	ins[11] = Pin_Get(ARM_P2_3);
+	ins[12] = Pin_Get(ARM_P2_4);
+	ins[13] = Pin_Get(ARM_P2_5);
+	ins[14] = Pin_Get(ARM_P0_16);
+	ins[15] = Pin_Get(ARM_P0_15);
+
+	Pin_Output(debug);
+	for (j = start_config; j < config_ints; j++) {
+		Pin_Output(ins[j]);
+		Pin_Int_Attach(ins[j], count, IOIntFall);
+	}
+
+	i = 0;
     while(1) {
-		Pin_On(in);
-		Pin_Off(in);
+		for (j = start_active; j < active_ints; j++) {
+			Pin_On(ins[j]);
+			Pin_Off(ins[j]);
+		}
     }
 }
