5a095ed682d795624432cfd3aec864551dd912c3
Margarita Manterola <margamanterola@gmail.com>
2012-06-10 19:06:24 -0300

    Remove unused constants in serial port driver

--- src/ports/lpc17xx/serial.c
+++ src/ports/lpc17xx/serial.c
@@ -60,101 +60,12 @@ const static int uarts[] = { UART_0, UART_1, UART_2, UART_3 };
 #define UART_LCR_BREAK_EN       ((uint8_t)(1<<6))     // UART Transmission Break enable 
 #define UART_LCR_DLAB_EN        ((uint8_t)(1<<7))     // UART Divisor Latches Access bit enable
 
+#define UART_TX_FIFO_SIZE       (16)                  // Size of the FIFO queue
 #define UART_BLOCK_TIMEOUT      0xFFFFFFFF            // Timeout for blocking 
 
 // Transmiter Holding Register Empty
 #define UART_IER_THREINT_EN     ((uint32_t)(1<<1))  // THR Interrupt enable
 
-
-#define UART_IER_RBRINT_EN      ((uint32_t)(1<<0))  // RBR Interrupt enable
-#define UART_IER_RLSINT_EN      ((uint32_t)(1<<2))  // RX line status interrupt enable
-#define UART1_IER_MSINT_EN      ((uint32_t)(1<<3))  // Modem status interrupt enable 
-#define UART1_IER_CTSINT_EN     ((uint32_t)(1<<7))  // CTS1 signal transition interrupt enable 
-#define UART_IER_ABEOINT_EN     ((uint32_t)(1<<8))  // Enables the end of auto-baud interrupt 
-#define UART_IER_ABTOINT_EN     ((uint32_t)(1<<9))  // Enables the auto-baud time-out interrupt 
-#define UART_IER_BITMASK        ((uint32_t)(0x307)) // UART interrupt enable register bit mask 
-#define UART1_IER_BITMASK       ((uint32_t)(0x38F)) // UART1 interrupt enable register bit mask 
-#define UART_IIR_INTSTAT_PEND   ((uint32_t)(1<<0))  //Interrupt Status - Active low 
-#define UART_IIR_INTID_RLS      ((uint32_t)(3<<1))  //Interrupt identification: Receive line status
-#define UART_IIR_INTID_RDA      ((uint32_t)(2<<1))  //Interrupt identification: Receive data available
-#define UART_IIR_INTID_CTI      ((uint32_t)(6<<1))  //Interrupt identification: Character time-out indicator
-#define UART_IIR_INTID_THRE     ((uint32_t)(1<<1))  //Interrupt identification: THRE interrupt
-#define UART1_IIR_INTID_MODEM   ((uint32_t)(0<<1))  //Interrupt identification: Modem interrupt
-#define UART_IIR_INTID_MASK     ((uint32_t)(7<<1))  //Interrupt identification: Interrupt ID mask 
-#define UART_IIR_FIFO_EN        ((uint32_t)(3<<6))  //These bits are equivalent to UnFCR[0] 
-#define UART_IIR_ABEO_INT       ((uint32_t)(1<<8))  // End of auto-baud interrupt 
-#define UART_IIR_ABTO_INT       ((uint32_t)(1<<9))  // Auto-baud time-out interrupt 
-#define UART_IIR_BITMASK        ((uint32_t)(0x3CF)) // UART interrupt identification register bit mask 
-
-#define UART_FCR_DMAMODE_SEL    ((uint8_t)(1<<3))   // UART DMA mode selection 
-#define UART_FCR_TRG_LEV0       ((uint8_t)(0))      // UART FIFO trigger level 0: 1 character 
-#define UART_FCR_TRG_LEV1       ((uint8_t)(1<<6))   // UART FIFO trigger level 1: 4 character 
-#define UART_FCR_TRG_LEV2       ((uint8_t)(2<<6))   // UART FIFO trigger level 2: 8 character 
-#define UART_FCR_TRG_LEV3       ((uint8_t)(3<<6))   // UART FIFO trigger level 3: 14 character 
-#define UART_FCR_BITMASK        ((uint8_t)(0xCF))   // UART FIFO control bit mask 
-#define UART_TX_FIFO_SIZE       (16)
-
-#define UART1_MCR_DTR_CTRL      ((uint8_t)(1<<0))       /*!< Source for modem output pin DTR */
-#define UART1_MCR_RTS_CTRL      ((uint8_t)(1<<1))       /*!< Source for modem output pin RTS */
-#define UART1_MCR_LOOPB_EN      ((uint8_t)(1<<4))       /*!< Loop back mode select */
-#define UART1_MCR_AUTO_RTS_EN   ((uint8_t)(1<<6))       /*!< Enable Auto RTS flow-control */
-#define UART1_MCR_AUTO_CTS_EN   ((uint8_t)(1<<7))       /*!< Enable Auto CTS flow-control */
-#define UART1_MCR_BITMASK       ((uint8_t)(0x0F3))      /*!< UART1 bit mask value */
-
-#define UART_LSR_OE         ((uint8_t)(1<<1))   /*!<Line status register: Overrun error*/
-#define UART_LSR_PE         ((uint8_t)(1<<2))   /*!<Line status register: Parity error*/
-#define UART_LSR_FE         ((uint8_t)(1<<3))   /*!<Line status register: Framing error*/
-#define UART_LSR_BI         ((uint8_t)(1<<4))   /*!<Line status register: Break interrupt*/
-#define UART_LSR_TEMT       ((uint8_t)(1<<6))   /*!<Line status register: Transmitter empty*/
-#define UART_LSR_RXFE       ((uint8_t)(1<<7))   /*!<Error in RX FIFO*/
-#define UART_LSR_BITMASK    ((uint8_t)(0xFF))   /*!<UART Line status bit mask */
-
-#define UART1_MSR_DELTA_CTS     ((uint8_t)(1<<0))   /*!< Set upon state change of input CTS */
-#define UART1_MSR_DELTA_DSR     ((uint8_t)(1<<1))   /*!< Set upon state change of input DSR */
-#define UART1_MSR_LO2HI_RI      ((uint8_t)(1<<2))   /*!< Set upon low to high transition of input RI */
-#define UART1_MSR_DELTA_DCD     ((uint8_t)(1<<3))   /*!< Set upon state change of input DCD */
-#define UART1_MSR_CTS           ((uint8_t)(1<<4))   /*!< Clear To Send State */
-#define UART1_MSR_DSR           ((uint8_t)(1<<5))   /*!< Data Set Ready State */
-#define UART1_MSR_RI            ((uint8_t)(1<<6))   /*!< Ring Indicator State */
-#define UART1_MSR_DCD           ((uint8_t)(1<<7))   /*!< Data Carrier Detect State */
-#define UART1_MSR_BITMASK       ((uint8_t)(0xFF))   /*!< MSR register bit-mask value */
-#define UART_SCR_BIMASK     ((uint8_t)(0xFF))   /*!< UART Scratch Pad bit mask */
-#define UART_ACR_START              ((uint32_t)(1<<0))  /**< UART Auto-baud start */
-#define UART_ACR_MODE               ((uint32_t)(1<<1))  /**< UART Auto baudrate Mode 1 */
-#define UART_ACR_AUTO_RESTART       ((uint32_t)(1<<2))  /**< UART Auto baudrate restart */
-#define UART_ACR_ABEOINT_CLR        ((uint32_t)(1<<8))  /**< UART End of auto-baud interrupt clear */
-#define UART_ACR_ABTOINT_CLR        ((uint32_t)(1<<9))  /**< UART Auto-baud time-out interrupt clear */
-#define UART_ACR_BITMASK            ((uint32_t)(0x307)) /**< UART Auto Baudrate register bit mask */
-#define UART_ICR_IRDAEN         ((uint32_t)(1<<0))          /**< IrDA mode enable */
-#define UART_ICR_IRDAINV        ((uint32_t)(1<<1))          /**< IrDA serial input inverted */
-#define UART_ICR_FIXPULSE_EN    ((uint32_t)(1<<2))          /**< IrDA fixed pulse width mode */
-#define UART_ICR_PULSEDIV(n)    ((uint32_t)((n&0x07)<<3))   /**< PulseDiv - Configures the pulse when FixPulseEn = 1 */
-#define UART_ICR_BITMASK        ((uint32_t)(0x3F))          /*!< UART IRDA bit mask */
-#define UART_FDR_DIVADDVAL(n)   ((uint32_t)(n&0x0F))        /**< Baud-rate generation pre-scaler divisor */
-#define UART_FDR_MULVAL(n)      ((uint32_t)((n<<4)&0xF0))   /**< Baud-rate pre-scaler multiplier value */
-#define UART_FDR_BITMASK        ((uint32_t)(0xFF))          /**< UART Fractional Divider register bit mask */
-#define UART_TER_BITMASK        ((uint8_t)(0x80))       /**< UART Transmit Enable Register bit mask */
-
-#define UART1_RS485CTRL_NMM_EN      ((uint32_t)(1<<0))  /*!< RS-485/EIA-485 Normal Multi-drop Mode (NMM)
-is disabled */
-#define UART1_RS485CTRL_RX_DIS      ((uint32_t)(1<<1))  /*!< The receiver is disabled */
-#define UART1_RS485CTRL_AADEN       ((uint32_t)(1<<2))  /*!< Auto Address Detect (AAD) is enabled */
-#define UART1_RS485CTRL_SEL_DTR     ((uint32_t)(1<<3))  /*!< If direction control is enabled
-(bit DCTRL = 1), pin DTR is used for direction control */
-#define UART1_RS485CTRL_DCTRL_EN    ((uint32_t)(1<<4))  /*!< Enable Auto Direction Control */
-#define UART1_RS485CTRL_OINV_1      ((uint32_t)(1<<5))  /*!< This bit reverses the polarity of the direction
-control signal on the RTS (or DTR) pin. The direction control pin
-    will be driven to logic "1" when the transmitter has data to be sent */
-
-#define UART1_RS485CTRL_BITMASK     ((uint32_t)(0x3F))  /**< RS485 control bit-mask value */
-
-#define UART1_RS485ADRMATCH_BITMASK ((uint8_t)(0xFF))   /**< Bit mask value */
-
-#define UART1_RS485DLY_BITMASK      ((uint8_t)(0xFF))   /** Bit mask value */
-
-#define UART_FIFOLVL_RXFIFOLVL(n)   ((uint32_t)(n&0x0F))        /**< Reflects the current level of the UART receiver FIFO */
-#define UART_FIFOLVL_TXFIFOLVL(n)   ((uint32_t)((n>>8)&0x0F))   /**< Reflects the current level of the UART transmitter FIFO */
-#define UART_FIFOLVL_BITMASK        ((uint32_t)(0x0F0F))        /**< UART FIFO Level Register bit mask */
 
 static uint8_t serial_initialized[] = { 0, 0, 0, 0 };
 
